465881bf6257cb9cbb1bf63fdff502785425be42
Flaco78 <122921589+Flaco78@users.noreply.github.com>
2024-04-22 12:17:11 +0300

    Update StartGui.java

--- src/main/java/com/example/gkl/StartGui.java
+++ src/main/java/com/example/gkl/StartGui.java
@@ -1,23 +1,23 @@
-// package com.example.gkl;
+package com.example.gkl;
 
-// import javafx.application.Application;
-// import javafx.fxml.FXMLLoader;
-// import javafx.scene.Scene;
-// import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-// import java.io.IOException;
+import java.io.IOException;
 
-// public class StartGui extends Application {
-//     @Override
-//     public void start(Stage stage) throws IOException {
-//         FXMLLoader fxmlLoader = new FXMLLoader(StartGui.class.getResource("login.fxml"));
-//         Scene scene = new Scene(fxmlLoader.load());
-//         stage.setTitle("Shop");
-//         stage.setScene(scene);
-//         stage.show();
-//     }
-//     public static void main(String[] args) {
-//         launch();
-//     }
-// }
+public class StartGui extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StartGui.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Shop");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
 
